4e4a75104963f7eacecec702c8eb48fe398b5634
jjg-git <john_joseph_giron@dlsu.edu.ph>
2023-10-09 12:44:30 +0800

    Fixed SokoBot.java

--- SokoBot.java
+++ SokoBot.java
@@ -2,7 +2,8 @@ package solver;
 
 public class SokoBot {
 
-   private int[2] playerCoordinate;
+//   private int[2] playerCoordinate;
+   private int[] playerCoordinate = new int[2];
    private int numOfGoals = 0;
    private int[][] goals;
    private int numOfCrates = 0;
@@ -10,27 +11,32 @@ public class SokoBot {
    private int numOfWalls = 0;
    private int[][] walls;
    private boolean up = true, down = true, left = true, right = true;
-   private int move = 1, prevMove;
-
+   private int move = 1;
+   
+   // Array indeces
+   private final int X = 0;
+   private final int Y = 1;
+   
    /*checks if player can move*/
-   public void checkWall(){
+   public void checkWall() {
       for (int checking = 0; checking < walls.length; checking++){
-         if (playerCoordinate[0]+move == walls[checking][0]){ //wall right of player
+         if (playerCoordinate[X]+move == walls[checking][X]){ //wall right of player
             right = false;
          }
-         if (playerCoordinate[1]+move == walls[checking][1]){ //wall above player
+         if (playerCoordinate[Y]+move == walls[checking][Y]){ //wall above player
             up = false;
          }
-         if (playerCoordinate[0]-move == walls[checking][0]){ //wall left of player
+         if (playerCoordinate[X]-move == walls[checking][X]){ //wall left of player
             left = false;
          }
-         if (playerCoordinate[1]-move == walls[checking][1]){ //wall below player
+         if (playerCoordinate[Y]-move == walls[checking][Y]){ //wall below player
             down = false;
          }
       }
+   }
 
       /*Checks if crates can move*/
-      public void checkCrate(){
+    public void checkCrate(){
          for (int checking = 0; checking < crates.length; checking++){
          if (playerCoordinate[0]+move == crates[checking][0]){ //crate right of player
             move = 2;
@@ -47,43 +53,17 @@ public class SokoBot {
          switch(move){
             case 2: checkWall();
          }
-      }
-      
+        }
+   }
 
   /*Responsible for the movement of player*/
-  public String movement(int prevMove){
-      checkWall();
-      checkCrate();
-     
-      switch(prevMove){
-         case 0: up = false;break;
-         case 1: down = false;break;
-         case 2: left = false;break;
-         case 3: right = false; break;
-      }
-     
-      if(up == true){
-         playerCoordinate[1]++;
-         prevMove = 0;
-         movement(prevMove);
-      }
-      if(down == true){
-         playerCoordinate[1]--;
-         prevMove = 1;
-         movement(prevMove);
-         
-      }
-      if(left == true){
-         playerCoordinate[0]--;
-         prevMove = 2;
-         movement(prevMove);
-      }
-      if(right == true){
-         playerCoordinate[0]++;
-         prevMove = 3;
-         movement(prevMove);
-      }
+  public void movement(){
+     while(goals.length > 0){
+        checkWall();
+        checkCrate();
+     }
   }
+
   public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
     /*
      * YOU NEED TO REWRITE THE IMPLEMENTATION OF THIS METHOD TO MAKE THE BOT SMARTER
@@ -95,33 +75,39 @@ public class SokoBot {
      */
    
     /*To identify the coordinates of all solid objects that is not an empty space*/
-    for(int i = 0; i < height; i++){ 
-      for(int j = 0; j < width; j++){
-        if(itemsData[i][j] == '@'){ //coordinates of player at start
-          playerCoordinate[0] = j;
-          playerCoordinate[1] = i;
-        }
-        if(itemsData[i][j] == '$'){ //coordinates of crates/boxes
-          crates[crates.length][0] = j;
-          crates[crates.length][1] = i;
-        }
-        if(mapData[i][j] == '.'){ //coordinates of goals
-          goals[goals.length][0] = j;
-          goals[goals.length][1] = i;
-        }
-        if(mapData[i][j] == '#'){ //coordinates of walls
-          walls[walls.length][0] = j;
-          walls[walls.length][1] = i;
-        }
-      }
-    }
-    prevMove = -1;
-    movement(prevMove);
+	  
+	  
+	  for(int i = 0; i < height; i++){
+       for(int j = 0; j < width; j++){
+         if(itemsData[i][j] == '@'){ //coordinates of player at start
+           playerCoordinate[0] = j;
+           playerCoordinate[1] = i;
+         }
+         if(itemsData[i][j] == '$'){ //coordinates of crates/boxes
+//           crates[crates.length][0] = j;
+//           crates[crates.length][1] = i;
+           crates[numOfCrates][0] = j;
+           crates[numOfCrates][1] = i;
+           numOfCrates++;
+         }
+         if(mapData[i][j] == '.'){ //coordinates of goals
+           goals[numOfGoals][0] = j;
+           goals[numOfGoals][1] = i;
+           numOfGoals++;
+         }
+         if(mapData[i][j] == '#'){ //coordinates of walls
+           walls[walls.length][0] = j;
+           walls[walls.length][1] = i;
+         }
+       }
+     }
+    
     try {
       Thread.sleep(3000);
     } catch (Exception ex) {
       ex.printStackTrace();
     }
+
     return "lrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlr";
   }
 
