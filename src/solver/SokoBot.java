c6706e375f6167f4ba146c7f3ee919b2a985b344
jjg-git <john_joseph_giron@dlsu.edu.ph>
2023-10-12 15:46:51 +0800

    Movements and collisions

--- src/solver/SokoBot.java
+++ src/solver/SokoBot.java
@@ -3,222 +3,78 @@ package solver;
 import gui.GamePanel;
 
 public class SokoBot {
-//   private int[2] playerCoordinate;
-   private int[] playerCoordinate = new int[2];
-   private int numOfGoals = 0;
-   private int[][] goals;
-   private int numOfCrates = 0;
-   private int[][] crates;
-   private int numOfWalls = 0;
-   private int[][] walls;
-   private boolean up = true, down = true, left = true, right = true;
-   private int moves = 0;
-   private int ptRow = -1;
-   private int ptCol = -1;
-   private int btRow = -1;
-   private int btCol = -1;
-   
-   // Array indeces
-   private final int X = 0;
-   private final int Y = 1;
+	// Array indexes for position
+	private final int X = 0;
+	private final int Y = 1;
+	
+	private int[] getPosOfChar(char[][] data, char target) {
+		int position[] = new int[2];
 
-    private void executeMove(int direction) {
-    if (direction == 0) {
-      this.ptRow = playerRow - 1;
-      this.ptCol = playerColumn;
-      this.btRow = playerRow - 2;
-      this.btCol = playerColumn;
-    } else if (direction == 1) {
-      this.ptRow = playerRow + 1;
-      this.ptCol = playerColumn;
-      this.btRow = playerRow + 2;
-      this.btCol = playerColumn;
-    } else if (direction == 2) {
-      this.ptRow = playerRow;
-      this.ptCol = playerColumn - 1;
-      this.btRow = playerRow;
-      this.btCol = playerColumn - 2;
-    } else if (direction == 3) {
-      this.ptRow = playerRow;
-      this.ptCol = playerColumn + 1;
-      this.btRow = playerRow;
-      this.btCol = playerColumn + 2;
-    }
-  }
+		/*****************************
+		 * Note:
+		 * Going through *rows* means changing *y* coordinates, and
+		 * Going through *columns* means changing *x* coordinates.
+		 * 
+		 * Therefore, accessing array element with x and y goes like this:
+		 * 		data[y][x]
+		 ***************************** 
+		 */
+		// Rows (y)
+		for (int y = 0; y < data.length; y++) {
+			// Columns (x)
+			for (int x = 0; x < data[y].length; x++) {
+				if (data[y][x] == target) {
+					position[X] = x;
+					position[Y] = y;
+					break;
+				}
+			}
+		}
+		
+		return position;
+	}
 
-  private void handleMovement(int ptRow, int ptCol, int btRow, int btCol) {
-    if (ptRow < 0 || ptRow >= rows || ptCol < 0 || ptCol >= columns) {
-      return;
-    }
-    if (map[ptRow][ptCol] == '#') {
-      return;
-    }
-    if (items[ptRow][ptCol] != '$') {
-      items[playerRow][playerColumn] = ' ';
-      items[ptRow][ptCol] = '@';
-      playerRow = ptRow;
-      playerColumn = ptCol;
-    } else if (items[ptRow][ptCol] == '$') {
-      if (btRow < 0 || btRow >= rows || btCol < 0 || btCol >= columns) {
-        return;
-      }
-      if (map[btRow][btCol] == '#' || items[btRow][btCol] == '$') {
-        return;
-      }
-      if (map[btRow][btCol] == '.') {
-        progress++;
-      }
-      if (map[ptRow][ptCol] == '.') {
-        progress--;
-      }
-      items[btRow][btCol] = '$';
-      items[playerRow][playerColumn] = ' ';
-      items[ptRow][ptCol] = '@';
-      playerRow = ptRow;
-      playerColumn = ptCol;
-    }
-
-    moves++;
-    handleMovement(ptRow, ptCol, btRow, btCol);
-  }
-  
-   /*checks if player can move
-   public void checkWall() {
-      for (int checking = 0; checking < walls.length; checking++){
-         if (playerCoordinate[X]+move == walls[checking][X]){ //wall right of player
-            right = false;
-         }
-         if (playerCoordinate[Y]+move == walls[checking][Y]){ //wall above player
-            up = false;
-         }
-         if (playerCoordinate[X]-move == walls[checking][X]){ //wall left of player
-            left = false;
-         }
-         if (playerCoordinate[Y]-move == walls[checking][Y]){ //wall below player
-            down = false;
-         }
-      }
-   }
-*/
-      /*Checks if crates can move
-
-   public void checkCrate(){
-      for (int checking = 0; checking < crates.length; checking++){
-         if (playerCoordinate[0]+move == crates[checking][0]){ //crate right of player
-            move = 2;
-         }
-         if (playerCoordinate[1]+move == crates[checking][1]){ //crate above player
-            move = 2;
-         }
-         if (playerCoordinate[0]-move == crates[checking][0]){ //crate left of player
-            move = 2;
-         }
-         if (playerCoordinate[1]-move == crates[checking][1]){ //crate below player
-            move = 2;
-         }
-         switch(move){ //to check for walls around the path of the crate
-            case 2: checkWall();
-         }
-      }
-   }
-
-   public void checkGoal(int[][]crates, int[][] goals){
-      for(int block = 0; block < crates.length; block++){
-         for(int goal = 0; goal < goals.length; goal++){
-            if (crates[block][0] == goals[goal][0] && crates[block][0] == goals[goal][0]){
-               for(int used = 0; used < visited.length; used++){
-                  if (visited[used][0] == 
-   }
-
-  /*Responsible for the movement of player
-  @param playerCoordinate coordinate of player
-  @param crates array of coordinates of crates
-  @param goals array of coordinates of goals
-  @param prevMove checks previous move made (-1 means no previous moves made)
-  */
-   /*
-  public String movement(int[] playerCoordinate, int[][] crates, int[][] goals, int prevMove){
-      checkWall(); //Checks for walls on all possible moves
-      checkCrate(); //Checks for crates on all possible moves and for walls next to those crates
-     
-      switch(prevMove){ //Checks for previous moves to avoid repeating moves
-         case 0: up = false;break;
-         case 1: down = false;break;
-         case 2: left = false;break;
-         case 3: right = false; break;
-      }
-     checkGoal(crates, goals);
-     //Enters the if statements where it can go
-      if(up == true){
-         playerCoordinate[1]++; //coordinate of player
-         prevMove = 0; //lets program know that this was the move made for the next move
-         movement(playerCoordinate, crates, goals, prevMove); //recursion for the next moves
-      }
-      if(down == true){
-         playerCoordinate[1]--;
-         prevMove = 1;
-         movement(playerCoordinate, crates, goals, prevMove);
-         
-      }
-      if(left == true){
-         playerCoordinate[0]--;
-         prevMove = 2;
-         movement(playerCoordinate, crates, goals, prevMove);
-      }
-      if(right == true){
-         playerCoordinate[0]++;
-         prevMove = 3;
-         movement(playerCoordinate, crates, goals, prevMove);
-      }
-  }
-
-*/
-  public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
-    /*
-     * YOU NEED TO REWRITE THE IMPLEMENTATION OF THIS METHOD TO MAKE THE BOT SMARTER
-     */
-    /*
-     * Default stupid behavior: Think (sleep) for 3 seconds, and then return a
-     * sequence
-     * that just moves left and right repeatedly.
-     */
-   
-    /*To identify the coordinates of all solid objects that is not an empty space
-    for(int i = 0; i < height; i++){ 
-      for(int j = 0; j < width; j++){
-        if(itemsData[i][j] == '@'){ //coordinates of player at start
-          playerCoordinate[0] = j;
-          playerCoordinate[1] = i;
-        }
-        if(itemsData[i][j] == '$'){ //coordinates of crates/boxes
-          crates[crates.length][0] = j;
-          crates[crates.length][1] = i;
-        }
-        if(mapData[i][j] == '.'){ //coordinates of goals
-          goals[goals.length][0] = j;
-          goals[goals.length][1] = i;
-        }
-        if(mapData[i][j] == '#'){ //coordinates of walls
-          walls[walls.length][0] = j;
-          walls[walls.length][1] = i;
-        }
-      }
-    }*/
-    while (int z < 4){
-         executeMove(z);
-         handleMovement(ptRow, ptCol, btRow, btCol);
-         z++;
-    }
-  //  prevMove = -1; //signifies that this is the first move to be made by player or that no moves have been previously made
-  //  movement(playerCoordinate, crates, goals, prevMove);
-
-    try {
-      Thread.sleep(3000);
-    } catch (Exception ex) {
-      ex.printStackTrace();
-    }
-
-    return "lrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlr";
-  }
-
+	private boolean checkSpace(char[][] mapData, char[][] itemsData, 
+							   int[] position) {
+		int x = position[X];
+		int y = position[Y];
+		// Check if it's a space or a goal. Whatever you can go through
+		if (mapData[y][x] == ' ' || mapData[y][x] == '.')
+			return true;
+		// It's a wall
+		else 
+			return false;
+	}
+	
+	public String search(int width, int height, 
+						 char[][] mapData, char[][] itemsData,
+						 int[] playerPos, String output) {		
+		// array directions
+		int[] upPos = playerPos;
+		int[] rightPos = playerPos;
+		int[] downPos = playerPos;
+		int[] leftPos = playerPos;
+		
+		upPos[Y] -= 1;
+		rightPos[X] += 1;
+		downPos[Y] += 1;
+		leftPos[X] -= 1;
+		
+		if (checkSpace(mapData, itemsData, upPos))
+			search(width, height, mapData, itemsData, upPos, output + "u");
+		if (checkSpace(mapData, itemsData, rightPos))
+			search(width, height, mapData, itemsData, rightPos, output + "r");
+		if (checkSpace(mapData, itemsData, downPos))
+			search(width, height, mapData, itemsData, downPos, output + "d");
+		if (checkSpace(mapData, itemsData, leftPos))
+			search(width, height, mapData, itemsData, leftPos, output + "l");
+		
+		return output;
+	}
+	
+	public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
+		int[] playerPos = getPosOfChar(itemsData, '@');
+		
+		return search(width, height, mapData, itemsData, playerPos, "");
+	}
 }
